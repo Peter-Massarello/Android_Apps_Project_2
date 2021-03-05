90252de6c5062dad63bb36edb6ea2d3108720091
Peter-Massarello <pmassarello@gmail.com>
2021-03-04 17:59:47 -0800

    First commit

---
+++ app/src/main/java/com/example/androidproject2/LapFragment.kt
@@ -0,0 +1,31 @@
+package com.example.androidproject2
+
+import androidx.lifecycle.ViewModelProvider
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class LapFragment : Fragment() {
+
+    companion object {
+        fun newInstance() = LapFragment()
+    }
+
+    private lateinit var viewModel: LapViewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.lap_fragment, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+        viewModel = ViewModelProvider(this).get(LapViewModel::class.java)
+        // TODO: Use the ViewModel
+    }
+
+}
---
+++ app/src/main/java/com/example/androidproject2/LapViewModel.kt
@@ -0,0 +1,7 @@
+package com.example.androidproject2
+
+import androidx.lifecycle.ViewModel
+
+class LapViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
